46922336926ce845765cf48f7ad6d7d788d7043b
fcamargovalencia <fabiancv_1025@outlook.com>
2019-02-09 21:13:31 -0500

    Seed implementation
    Datos iniciales para productos en la BD

---
+++ Shop.Web/Data/SeedDB.cs
@@ -0,0 +1,43 @@
+﻿namespace Shop.Web.Data
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Entities;
+
+    public class SeedDb
+    {
+        private readonly DataContext context;
+        private Random random;
+
+        public SeedDb(DataContext context)
+        {
+            this.context = context;
+            random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await context.Database.EnsureCreatedAsync();
+
+            if (!context.Products.Any())
+            {
+                AddProduct("iPhone X");
+                AddProduct("Magic Mouse");
+                AddProduct("iWatch Series 4");
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            context.Products.Add(new Product
+            {
+                Name = name,
+                Price = random.Next(1000),
+                IsAvailabe = true,
+                Stock = random.Next(100)
+            });
+        }
+    }
+}

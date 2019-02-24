0fc46457dadadb362761a9a6a237d7ab9b3bafb3
fcamargovalencia <fabiancv_1025@outlook.com>
2019-02-24 17:04:28 -0500

    MVVM Arquitecture
    Implementacion del patron MVVM en el proyecto xamarin e inicializacion de la loginPage

---
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,56 @@
+﻿namespace Shop.UIForms.ViewModels
+{
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Xamarin.Forms;
+
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public LoginViewModel()
+        {
+            this.Email = "fabiancv.90@gmail.com";
+            this.Password = "Arenacv9025*";
+        }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password",
+                    "Accept");
+                return;
+            }
+
+            if (!this.Email.Equals("fabiancv.90@gmail.com") || !this.Password.Equals("Arenacv9025*"))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "User or password wrong",
+                    "Accept");
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Fuck yeah!!!",
+                    "Accept");
+        }
+    }
+}
